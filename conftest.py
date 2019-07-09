415eea7bff2056d3676b14ee3688c68cc470e824
dron41192 <dron41192@mail.ru>
2019-07-09 14:15:30 +0300

    Добавлен тест

---
+++ conftest.py
@@ -0,0 +1,33 @@
+﻿import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en", help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    if browser_name == "chrome":
+        
+        #browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+        print("\nstart chrome browser for test..") 
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+
+        print("\nstart firefox browser for test..")
+      #  browser = webdriver.Firefox()
+    else:
+        print("Browser {} still is not implemented".format(browser_name))
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
